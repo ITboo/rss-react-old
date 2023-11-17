c4f98d5395d79b8de7ff84179292de7fc800bec0
ITboo <annaacdc@tut.by>
2023-11-17 19:09:49 +0300

    feat: add hooks and api handling

---
+++ src/app/redux/api/api.ts
@@ -0,0 +1,21 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+import { Character } from "../../types/types";
+import { API_URL } from "../../constants/constants";
+
+export const api = createApi({
+  reducerPath: "api",
+  baseQuery: fetchBaseQuery({
+    baseUrl: API_URL,
+  }),
+  endpoints: (build) => ({
+    getCharacterById: build.query<Character, number>({
+      query: (id: number) => `/${id}`,
+    }),
+    getCharacters: build.query<Character[], string>({
+      query: (name: string) => `/?name=${name}`,
+    }),
+  }),
+});
+
+export const { useGetCharactersQuery, useGetCharacterByIdQuery } = api;
