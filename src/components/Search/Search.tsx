ea8fedfa198abcb853d2735822553b476d065888
ITboo <annaacdc@tut.by>
2023-11-19 16:45:26 +0300

    feat: update search form and add autofocus

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,13 +1,27 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useAppDispatch, useAppSelector } from "../../app/redux/hooks/hooks";
 import { setSearchValue } from "../../app/redux/slices/searchSlice";
 
 import "./Search.css";
 
 function Search() {
-  const dispatch = useAppDispatch();
   const searchValue = useAppSelector((state) => state.search.value);
   const [value, setValue] = useState(searchValue);
+
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    if (inputRef.current) inputRef.current.focus();
+  }, []);
+
+  const handleOnChange: React.ChangeEventHandler<HTMLInputElement> = (e) => {
+    setValue(e.target.value);
+  };
+
+  const dispatch = useAppDispatch();
+  const handleOnClick: React.MouseEventHandler<HTMLButtonElement> = () => {
+    dispatch(setSearchValue(value));
+  };
 
   return (
     <form
@@ -22,9 +36,11 @@ function Search() {
         type="text"
         placeholder="Search..."
         data-testid="search"
-        onChange={(e) => setValue(e.target.value)}
+        onChange={handleOnChange}
         value={value}
+        ref={inputRef}
       />
+      <button onClick={handleOnClick}>Search</button>
     </form>
   );
 }
